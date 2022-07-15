e7c6dc33524a5935c737bf65705f2e7da6cf391f
Lucas Fernando Macedo Pereira <lucasfernandomacedo13@gmail.com>
2022-07-15 20:23:33 -0300

    requirement 03

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../helpers/renderWithRouter';
+import { FavoritePokemons } from '../pages';
+import data from '../data';
+
+describe('3º - Favorite Pokemons Component', () => {
+  it('shows the message "No favorite pokemon found", if no pokemon has been added',
+    () => {
+      renderWithRouter(<FavoritePokemons />);
+      const paragraph = screen.getByText(/No favorite pokemon found/i);
+      expect(paragraph).toBeInTheDocument();
+    });
+
+  it('contains all the favorite pokemons cards', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ [data[0], data[1]] } />);
+    const listOfPokemons = screen.getAllByTestId('pokemon-name');
+    expect(listOfPokemons).toHaveLength(2);
+  });
+});
