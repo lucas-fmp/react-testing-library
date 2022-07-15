7f3ab4d4a5af51a7395c6bc79e8600ba2e83e11f
Lucas Fernando Macedo Pereira <lucasfernandomacedo13@gmail.com>
2022-07-15 20:34:07 -0300

    requirement 04

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../helpers/renderWithRouter';
+import { NotFound } from '../pages';
+
+describe('1º - App Component', () => {
+  it('contains an h2 heading with the text "Page requested not found"', () => {
+    renderWithRouter(<NotFound />);
+    const heading = screen.getByRole('heading', { name: /Page requested not found/i });
+    expect(heading).toBeInTheDocument();
+  });
+  it('contains the correct image', () => {
+    renderWithRouter(<NotFound />);
+    const URL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const image = screen.getAllByRole('img')[1];
+    expect(image.src).toBe(URL);
+  });
+});
